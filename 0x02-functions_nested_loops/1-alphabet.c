a5e897863e3c037da75a86732adc22e6b29284ab
henryokey <henryokey88@gmail.com>
2022-09-14 13:54:44 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,13 @@
  **/
 void print_alphabet(void)
 {
-	char i;
+	char c ='a';
+	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+
+	for (i = '0'; i < '26'; i++)
+	{
+		_putchar(c + i);
+	}
+	_putchar('10');
 }
