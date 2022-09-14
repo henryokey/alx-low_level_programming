4a0986615a6f25a24a57c9d998ed98c00443a44e
henryokey <henryokey88@gmail.com>
2022-09-14 14:58:33 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-			n++
+			n++;
 		}
 	}
 	else if (n > 98)
@@ -29,7 +29,7 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-			n--
+			n--;
 		}
 	}
 	else
